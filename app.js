aad712704ca0855dd95156e734dcab4de25864ff
synnestorm <synne.storm@gmail.com>
2026-05-31 22:52:25 +0200

    feat: router() if else statement

--- app.js
+++ app.js
@@ -3,4 +3,11 @@ const aboutPage = document.getElementById("about-page");
 
 function router() {
   const currentUrl = window.location.hash || "#projects";
+  if (currentUrl === "#about") {
+    projectsPage.classList.add("hidden");
+    aboutPage.classList.remove("hidden");
+  } else {
+    projectsPage.classList.remove("hidden");
+    aboutPage.classList.add("hidden");
+  }
 }
