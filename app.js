15124f8ef46eb72c500120816ba5254bb75714d9
synnestorm <synne.storm@gmail.com>
2026-05-31 23:09:39 +0200

    add: addEventListener + changed currentUrl to currentHash

--- app.js
+++ app.js
@@ -1,13 +1,19 @@
 const projectsPage = document.getElementById("projects-page");
 const aboutPage = document.getElementById("about-page");
+const introduction = document.querySelector(".introduction");
 
 function router() {
-  const currentUrl = window.location.hash || "#projects";
-  if (currentUrl === "#about") {
+  const currentHash = window.location.hash || "#projects";
+  if (currentHash === "#about") {
     projectsPage.classList.add("hidden");
     aboutPage.classList.remove("hidden");
+    introduction.classList.add("hidden");
   } else {
     projectsPage.classList.remove("hidden");
     aboutPage.classList.add("hidden");
+    introduction.classList.remove("hidden");
   }
 }
+
+window.addEventListener("hashchange", router);
+window.addEventListener("load", router);
